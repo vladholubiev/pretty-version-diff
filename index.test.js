a95fdeac87c11c1d5da8b3a20e776f8c3c01c90c
Vlad Holubiev <golubev.vld@gmail.com>
2018-01-27 22:01:25 +0200

    test: strip ansi in tests so they pass in ci

--- index.test.js
+++ index.test.js
@@ -1,3 +1,4 @@
+const stripAnsi = require('strip-ansi');
 const prettyVersionDiff = require('.');
 
 it('should export a prettyVersionDiff function', () => {
@@ -6,20 +7,20 @@ it('should export a prettyVersionDiff function', () => {
 
 it('should return incremented patch version', () => {
   const versionDiff = prettyVersionDiff('1.0.1', 'patch');
-  expect(versionDiff).toEqual('1.0.2');
+  expect(stripAnsi(versionDiff)).toEqual('1.0.2');
 });
 
 it('should return incremented minor version', () => {
   const versionDiff = prettyVersionDiff('1.0.1', 'minor');
-  expect(versionDiff).toEqual('1.1.0');
+  expect(stripAnsi(versionDiff)).toEqual('1.1.0');
 });
 
 it('should return incremented major version', () => {
   const versionDiff = prettyVersionDiff('1.0.1', 'major');
-  expect(versionDiff).toEqual('2.0.0');
+  expect(stripAnsi(versionDiff)).toEqual('2.0.0');
 });
 
 it('should return incremented prerelease version', () => {
   const versionDiff = prettyVersionDiff('1.0.1', 'prerelease');
-  expect(versionDiff).toEqual('1.0.2-0');
+  expect(stripAnsi(versionDiff)).toEqual('1.0.2-0');
 });
